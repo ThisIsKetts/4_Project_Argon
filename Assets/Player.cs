4199643d049c827873f56eb28b1feda37ff12b8b
ThisIsKetts <ketsuruigfx@gmail.com>
2018-02-03 17:17:50 +0000

    Section 4 Lecture 85. Added movement to the ship with a player script. Attached the ship to the main camera so the ship "Flies" around.

---
+++ Assets/Player.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+   [Tooltip ("In ms^-1")] [SerializeField] float speed = 20f;
+   [Tooltip("In m")] [SerializeField] float xRange = 5f;
+   [Tooltip("In m")] [SerializeField] float yRange = 4f;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HorizontalMovement();
+        VerticalMovement();
+
+    }
+
+    private void VerticalMovement()
+    {
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yThrow * speed * Time.deltaTime;
+
+        float rawYPos = transform.localPosition.y + yOffset;
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, +yRange);
+
+        transform.localPosition = new Vector3(transform.localPosition.x, clampedYPos, transform.localPosition.z);
+    }
+
+    private void HorizontalMovement()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+
+        transform.localPosition = new Vector3(clampedXPos, transform.localPosition.y, transform.localPosition.z);
+    }
+}
